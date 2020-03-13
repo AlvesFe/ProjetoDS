368d3d1da1814a842c327fbc51a3623436e99d3e
BuildTools <felipe.alves135@etec.sp.gov.br>
2020-03-13 15:13:52 -0300

    Adição de classe conexão com banco de dados

---
+++ DAL_ProjetoDS_2020/Conexao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL_ProjetoDS_2020
+{
+    class Conexao
+    {
+
+        public static SqlConnection Conectar()
+        {
+			try
+			{
+				SqlConnection conn = new SqlConnection((@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProjetoDS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+				conn.Open();
+				return conn;
+
+			}
+			catch
+			{
+
+				throw new Exception("Não foi possivel conectar. Tente Novamente!");
+
+			}
+        }
+
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE ProjetoDS;
+
+USE ProjetoDS;
